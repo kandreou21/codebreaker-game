e48faface374c8c5e3f847e32a4e52358147476a
kandreou21 <72383375+kandreou21@users.noreply.github.com>
2023-06-23 19:48:51 +0300

    Add files via upload

--- codebreaker.py
+++ codebreaker.py
@@ -44,46 +44,62 @@ def evaluate_guess(code, guess):
     return evaluation
 
 
-while True:
-    game = int(input("Input 1 for 1-player game or 2 for 2-player game: "))
-    if game == 1 or game == 2:
-        break
-    else:
-        print("Error, Input 1 for 1-player game or 2 for 2-player game")
-
-if game == 1:
-    code = generate_random_code()
-else:
-    print("\nPlayer 2 enter the secret code (4 colors).")
+def choose_game():
     while True:
-        code = input("You can use any combination of 4 symbols in ['1', '2', '3', '4', '5', '6'] as colors: ")
-        if len(code) != 4:
-            print("The secret code has exactly four colors. Try again!")
-            continue
-        if is_code_valid(code):
-            break
-
-    clear = lambda: os.system('cls')  # clear console after player enters code
-    clear()
-
-print("Player 1 please, enter your color code.")
-print("You can use any combination of 4 symbols in ['1', '2', '3', '4', '5', '6'] as colors")
-
-tries = 8
-for i in range(tries):
-    while True:
-        print("Attempt", i+1)
-        guess = input("")
-        if len(guess) != 4:
-            print("The secret code has exactly four colors. Try again!")
-            continue
-        if is_code_valid(guess):
+        game = int(input("Input 1 for 1-player game or 2 for 2-player game: "))
+        if game == 1 or game == 2:
             break
         else:
-            print("You can only use ['1', '2', '3', '4', '5', '6'] as colors. Try again!")
+            print("Error, Input 1 for 1-player game or 2 for 2-player game")
+    return game
 
-    if code == guess:
-        print("Congratulations! You found it in ", i+1, "attempts!")
-        break
+
+def enter_secret_code():
+    code = ""
+    game = choose_game()
+    if game == 1:
+        code = generate_random_code()
     else:
-        print(evaluate_guess(code, guess))
+        print("\nPlayer 2 enter the secret code (4 colors).")
+        while True:
+            code = input("You can use any combination of 4 symbols in ['1', '2', '3', '4', '5', '6'] as colors: ")
+            if len(code) != 4:
+                print("The secret code has exactly four colors. Try again!")
+                continue
+            if is_code_valid(code):
+                break
+
+        clear = lambda: os.system('cls')  # clear console after player enters code
+        clear()
+    return code
+
+
+def main():
+    code = enter_secret_code()
+    print("Player 1 please, enter your color code.")
+    print("You can use any combination of 4 symbols in ['1', '2', '3', '4', '5', '6'] as colors")
+
+    tries = 8
+    for i in range(tries):
+        while True:
+            print("Attempt", i+1)
+            guess = input("")
+            if len(guess) != 4:
+                print("The secret code has exactly four colors. Try again!")
+                continue
+            if is_code_valid(guess):
+                break
+            else:
+                print("You can only use ['1', '2', '3', '4', '5', '6'] as colors. Try again!")
+
+        if code == guess:
+            print("Congratulations! You found it in ", i+1, "attempts!")
+            return
+        else:
+            print(evaluate_guess(code, guess))
+    print("You failed to guess within 8 attempts.")
+    print("The secret code was", code)
+
+
+if __name__ == "__main__":
+    main()
